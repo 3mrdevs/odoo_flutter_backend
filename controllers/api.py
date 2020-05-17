1487b09608463b7a6957695de90b804fff58cb09
Amr Abd-Alkrim Hassan <wizmoori@gmail.com>
2020-05-17 11:02:03 +0200

    Rename external id with the folder name of the app. plus renaming the views file to view

--- controllers/api.py
+++ controllers/api.py
@@ -6,3 +6,18 @@ from odoo.http import request
 
 class Api (http.Controller):
 
+    @http.route(['/api/books'], type="http", auth="public", website=True, method=['POST'], csrf=False)
+    def get_books(self):
+        values = {}
+
+        data = request.env['flutter.book'].sudo().search_read([("state", "=", "published")])
+
+        if data:
+            values['success'] = True
+            values['data'] = data
+        else:
+            values['success'] = False
+            values['error_code'] = 1
+            values['error_data'] = 'No data found!'
+
+        return json.dumps(values)
