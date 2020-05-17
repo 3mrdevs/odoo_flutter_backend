ac1a365239fe643e1f0bb2b3d5c9114af7812249
Amr Abd-Alkrim Hassan <wizmoori@gmail.com>
2020-05-17 04:08:37 +0200

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Odoo flutter back-end',
+    'version': '13.0.1',
+    'summary': 'Odoo flutter back-end example',
+    'description': 'Simple example of Odoo back-end for a flutter application with API controller',
+    'category': 'Flutter',
+    'author': '3mrdev',
+    'website': 'https://3mr.dev',
+    'license': 'LGPL-3',
+    # 'data': ['views/view.xml'],
+    # 'demo': ['data/demo.xml'],
+    'installable': True,
+    'auto_install': False
+}
