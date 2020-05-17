b7a2e2742bb33d896e2d5ca509b27a54e8acaa6c
Amr Abd-Alkrim Hassan <wizmoori@gmail.com>
2020-05-17 11:51:39 +0200

    Return the categories of the book in the JSONObject. and Looping through the data to get name because the categories are returned as a list. I changed the way to the search orm method because it returns object and left search_read because it returns list

--- controllers/api.py
+++ controllers/api.py
@@ -10,25 +10,30 @@ class Api (http.Controller):
     def get_books(self):
         values = {}
 
-        data = request.env['flutter.book'].sudo().search_read([],fields=[
-            "name",
-            "description",
-            "state",
-            "publish_date",
-            "language",
-            "isbn",
-            "price",
-            "author",
-        ])
+        data = request.env['flutter.book'].sudo().search([])
 
         if data:
-
-            for book in data:
-                book["publish_date"] = str(book["publish_date"])
-                book["author"] = book["author"][1]
+            books = []
+            for book_data in data:
+                book = {
+                    "name": str(book_data["name"]),
+                    "description": book_data["description"],
+                    "state": book_data["state"],
+                    "publish_date": str(book_data["publish_date"]),
+                    "language": book_data["language"],
+                    "isbn": book_data["isbn"],
+                    "price": book_data["price"],
+                    "author": book_data["author"]["name"],
+                    "publisher": book_data["publisher"]["name"],
+                }
+                categories = []
+                for category in book_data.categories:
+                    categories.append(category.name)
+                book["categories"] = categories
+                books.append(book)
 
             values['success'] = True
-            values['data'] = data
+            values['data'] = books
         else:
             values['success'] = False
             values['error_code'] = 1
