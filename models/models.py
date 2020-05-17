7869a3549acfe99cac83aac58e91fd28e27c499d
Amr Abd-Alkrim Hassan <wizmoori@gmail.com>
2020-05-17 04:41:17 +0200

    Writing Odoo model for storing books and doing CRUD operations.

---
+++ models/models.py
@@ -0,0 +1,11 @@
+from odoo import fields, models, api
+
+
+class Books (models.Model):
+    _name = 'flutter.book'
+    _description = 'Model for storing books and do CRUD operations.'
+
+    name = fields.Char()
+    
+
+
